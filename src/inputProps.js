e42faae9d48d26c862933b2966e32ced75270187
Philipp Binder <binder.philipp10@gmail.com>
2021-09-26 23:26:08 +0200

    Add array and <li>

--- src/inputProps.js
+++ src/inputProps.js
@@ -11,13 +11,25 @@ export function Def() {
 // export const function
 
 function FullName() {
-  const [firstName, setFirstName] = useState('First Name');
-  const [lastName, setLastName] = useState('Last Name');
-  const [fullName, setFullName] = useState('Full Name');
+  const [firstName, setFirstName] = useState('Placeholder: First Name');
+  const [lastName, setLastName] = useState('Placeholder: Last Name');
+  const [fullName, setFullName] = useState('Placeholder: Full Name');
   const testName = fullName;
-  const list = ['Test Name'];
+  const list = [];
+  // const [list, setList] = useState([]);
+  const allGuests = ['Test'];
   list.push(fullName);
-  console.log(list);
+  function firstFunction() {
+    list.push(setFullName(firstName + ' ' + lastName));
+  }
+  function secondFunction() {
+    allGuests.push(list);
+  }
+  function setterFunction() {
+    const showMe = list[0];
+    console.log(showMe);
+  }
+  // console.log(list);
   // function submit(first, last) {
   //   SetFullName = firstName + lastName;
   // }
@@ -36,20 +48,36 @@ function FullName() {
         onChange={(e) => setLastName(e.currentTarget.value)}
       />
       <button
-        onClick={() => list.push(setFullName(firstName + ' ' + lastName))}
+        // onClick={() => list.push(setFullName(firstName + ' ' + lastName))}
+        onClick={
+          () => {
+            firstFunction();
+            secondFunction();
+            setterFunction();
+          }
+          // sollte den Wert, also den neuen Namen vom user, in das 2te Array pushen
+          // (list = [])) // sollte das erste Array list wieder cleanen damit wieder die Inputfielder leer sind?
+        }
       >
         {' '}
         submit{' '}
       </button>{' '}
-      <p> {testName} </p>
-      {/* <br />
-      <br />
-      <br />
-      <ul> {name} </ul> */}
+      {/* <p> {list} </p> */}
+      <ul>
+        {list.map((newName) => {
+          return <li key={newName}> {newName}</li>;
+        })}
+      </ul>
     </>
   );
 }
+
+// https://www.youtube.com/watch?v=uBzgn2RQyYw&list=PLMZMRynGmhsix2_xWKX6sp4rDr0E1tIQ_&index=26 ca Minute 34
 // Idee: array und dann den Wert reinpushen?
+// Idee: onClick sets array to empty []? To clear?
+// 2 Arrays machen? Das erste ist das was der User eingibt, das 2te ist das, was die Namen anzeigt?
+// useEffect für das Verändern des Arrays?
+// destructuring von dem Array um den aktuellen Wert rauszunehmen?
 export default FullName;
 
 // export function showName() {}
